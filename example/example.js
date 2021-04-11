8eb09b383bfde8138f0af74de20c24cde2d407b6
Hanbyeol Jo <60456752+PinMilk@users.noreply.github.com>
2021-04-11 23:38:59 +0900

    Commit
    Commit

--- example/example.js
+++ example/example.js
@@ -1,6 +1,6 @@
 const Translator = require('translator').Translator;
 
 function response(room, msg, sender, igc, replier) {
-    replier.reply(new Translator().translate('en', 'ko', msg)); // Only result
-    replier.reply(new Translator().translate('en', 'ko', msg, true)); // Result by raw json
+    replier.reply(new Translator().translate('detect', 'ko', msg, true, false)); // Only result
+    replier.reply(new Translator().translate('en', 'ko', msg, true, true)); // Result by raw json
 }
