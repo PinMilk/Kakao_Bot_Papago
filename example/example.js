ad46837f76a9e3102a07f4a43d335726b816b44f
Hanbyeol Jo <hanbyeul04@kakao.com>
2021-04-17 01:35:44 +0900

    edit

--- example/example.js
+++ example/example.js
@@ -1,6 +1,11 @@
-const Translator = require('translator').Translator;
+const { Translator } = require('../lib/translator');
 
 function response(room, msg, sender, igc, replier) {
-    replier.reply(new Translator().translate('detect', 'ko', msg, true, false)); // Only result
-    replier.reply(new Translator().translate('en', 'ko', msg, true, true)); // Result by raw json
+    replier.reply(new Translator().translate('detect', 'ko', msg)); // Only result
+    replier.reply(new Translator().translate('en', 'ko', msg, {
+        verbose: true
+    })); // Result by raw json
+    replier.reply(new Translator().translate('en', 'ko', msg, {
+        honorfic: false
+    })); // No honorfic
 }
