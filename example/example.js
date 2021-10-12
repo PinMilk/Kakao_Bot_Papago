c15091bd25dfa0d941ae0f9eaa1ac4d02eb986f6
pinmilk <hanbyeul04@kakao.com>
2021-10-12 20:27:10 +0900

    added multi translation

--- example/example.js
+++ example/example.js
@@ -1,11 +1,19 @@
-const { Translator } = require('../lib/translator');
+const { Translator } = require('translator');
 
 function response(room, msg, sender, igc, replier) {
-    replier.reply(new Translator().translate('detect', 'ko', msg)); // Only result
+    // 언어 감지, 한국어로 번역 후 결과만 리턴
+    replier.reply(new Translator().translate('detect', 'ko', msg));
     replier.reply(new Translator().translate('en', 'ko', msg, {
+        // raw json으로 리턴
         verbose: true
-    })); // Result by raw json
+    }));
     replier.reply(new Translator().translate('en', 'ko', msg, {
+        // 경어 X
         honorfic: false
-    })); // No honorfic
+    }));
+    replier.reply(new Translator().translate('en', 'ko', msg, {
+        // 경어 X
+        honorfic: false
+    }));
+    replier.reply(new Translator().multiTranslate('en', 'ko', [msg, 'Good morning.']));
 }
