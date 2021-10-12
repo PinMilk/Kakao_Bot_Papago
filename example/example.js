8724e3a670f7544b02875b7557e42e62de07491a
pinmilk <hanbyeul04@kakao.com>
2021-10-12 20:39:10 +0900

    commit

--- example/example.js
+++ example/example.js
@@ -3,10 +3,10 @@ const { Translator } = require('translator');
 function response(room, msg, sender, igc, replier) {
     // 언어 감지, 한국어로 번역 후 결과만 리턴
     replier.reply(new Translator().translate('detect', 'ko', msg));
-    replier.reply(new Translator().translate('en', 'ko', msg, {
+    replier.reply(JSON.stringify(new Translator().translate('en', 'ko', msg, {
         // raw json으로 리턴
         verbose: true
-    }));
+    }), null, 4));
     replier.reply(new Translator().translate('en', 'ko', msg, {
         // 경어 X
         honorfic: false
@@ -15,5 +15,5 @@ function response(room, msg, sender, igc, replier) {
         // 경어 X
         honorfic: false
     }));
-    replier.reply(new Translator().multiTranslate('en', 'ko', [msg, 'Good morning.']));
+    replier.reply(JSON.stringify(new Translator().multiTranslate('en', 'ko', [msg, 'Good morning.']), null, 4));
 }
